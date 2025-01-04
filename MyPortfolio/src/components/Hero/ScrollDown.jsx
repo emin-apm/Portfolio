380ac17af05daa73c275f4e26ce7a28336dcd87c
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-01-05 00:37:02 +0200

    scroll down component

---
+++ MyPortfolio/src/components/Hero/ScrollDown.jsx
@@ -0,0 +1,12 @@
+import styles from "./HeroStyles.module.css";
+
+export default function ScrollDown() {
+  return (
+    <div className={styles.home_scroll}>
+      <a href="" className={`${styles.home_scroll_button} button_flex`}>
+        <i class="fa-solid fa-down-long"></i>
+      </a>
+      <span className={styles.home_scroll_name}>Scroll Down</span>
+    </div>
+  );
+}
