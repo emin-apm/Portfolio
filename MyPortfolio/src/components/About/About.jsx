522f1bc531a942ab29d8bc770de6308b3782e3e1
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-01-08 15:26:03 +0200

    About and info component

---
+++ MyPortfolio/src/components/About/About.jsx
@@ -0,0 +1,31 @@
+import styles from "./AboutStyles.module.css";
+import AboutImg from "../../assets/about_img.jpg";
+import Info from "./Info";
+
+export default function About() {
+  return (
+    <section className={`${styles.about} section`} id="about">
+      <h2 className="section_title">About Me</h2>
+      <span className="section_subtitles">My Introduction</span>
+      <div className={`${styles.about_container} container grid`}>
+        <img src={AboutImg} alt="" className={styles.about_img} />
+        <div className={styles.about_data}>
+          <Info />
+          <p className={styles.about_description}>
+            I am Emin, a passionate programmer with a comprehensive background
+            in JavaScript, having completed the rigorous curriculum at SoftUni
+            University. Over the past year, I have dedicated myself to honing my
+            technical skills and expanding my knowledge in JavaScript, React,
+            Angular, Node.js, MongoDB, and other cutting-edge technologies. I am
+            currently engaged in the Tech Lead Academy 2 internship, where I am
+            applying my skills to real-world projects and gaining invaluable
+            industry experience.
+            <br /> I'm not just into coding, I'm also a sports enthusiast! I'm
+            always open to new opportunities to expand my skill set.
+          </p>
+          <div className="button button_flex">Check CV</div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ MyPortfolio/src/components/About/Info.jsx
@@ -0,0 +1,5 @@
+import styles from "./AboutStyles.module.css";
+
+export default function Info() {
+  return <h1>info</h1>;
+}
