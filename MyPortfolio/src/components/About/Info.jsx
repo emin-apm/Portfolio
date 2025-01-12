bdb8d72d80bc57d5f214b40b0381868facebf6ac
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-01-12 18:34:28 +0200

    about info change

--- MyPortfolio/src/components/About/Info.jsx
+++ MyPortfolio/src/components/About/Info.jsx
@@ -6,7 +6,9 @@ export default function Info() {
       <div className={styles.about_box}>
         <i className={`${styles.about_icon} fa-solid fa-award`}></i>
         <h3 className={styles.about_title}>Experience</h3>
-        <span className={styles.about_subtitle}>1 years not professional</span>
+        <span className={styles.about_subtitle}>
+          1.5 years not professional
+        </span>
       </div>
 
       <div className={styles.about_box}>
