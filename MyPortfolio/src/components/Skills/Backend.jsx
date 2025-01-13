af54d9358ad85b573718515eab9a1392fedf2755
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-01-13 23:22:53 +0200

    backend data change

--- MyPortfolio/src/components/Skills/Backend.jsx
+++ MyPortfolio/src/components/Skills/Backend.jsx
@@ -1,5 +1,8 @@
 import styles from "./SkillsStyles.module.css";
 
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+
 export default function Backend() {
   return (
     <div className={styles.skill_content}>
@@ -7,75 +10,30 @@ export default function Backend() {
       <div className={styles.skill_box}>
         <div className={styles.skill_group}>
           <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
+            <i class="fa-brands fa-node-js"></i>
             <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
+              <h3 className={styles.skill_name}>Node.js</h3>
+              <span className={styles.skill_level}>Advanced</span>
             </div>
           </div>
           <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
+            <i>
+              <SiExpress />
+            </i>
             <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
-            </div>
-          </div>
-          <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
-            <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
-            </div>
-          </div>
-          <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
-            <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
-            </div>
-          </div>
-          <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
-            <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
+              <h3 className={styles.skill_name}>Express.JS</h3>
+              <span className={styles.skill_level}>Advanced</span>
             </div>
           </div>
         </div>
         <div className={styles.skill_group}>
           <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
+            <i>
+              <SiMongodb />
+            </i>
             <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
-            </div>
-          </div>
-          <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
-            <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
-            </div>
-          </div>
-          <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
-            <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
-            </div>
-          </div>
-          <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
-            <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
-            </div>
-          </div>
-          <div className={styles.skill_data}>
-            <i className="fa-brands fa-square-js"></i>
-            <div>
-              <h3 className={styles.skill_name}>Backend</h3>
-              <span className={styles.skill_level}></span>
+              <h3 className={styles.skill_name}>MongoDB</h3>
+              <span className={styles.skill_level}>Advanced</span>
             </div>
           </div>
         </div>
