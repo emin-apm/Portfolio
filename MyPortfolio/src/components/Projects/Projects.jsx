0b8d6db1825d6dd8130923530d368193ccb82352
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-01-16 14:39:44 +0200

    responsivenes to portfolio added

--- MyPortfolio/src/components/Projects/Projects.jsx
+++ MyPortfolio/src/components/Projects/Projects.jsx
@@ -68,6 +68,107 @@ export default function Projects() {
             </div>
           </div>
         </div>
+
+        <div className={styles.projects_content}>
+          <div>
+            <i class="fa-solid fa-image"></i>
+
+            <h3 className={styles.projects_title}>Title Here</h3>
+          </div>
+
+          <div className={styles.projects_buttons_container}>
+            <span className={styles.projects_button}>
+              GitHub
+              <i class="fa-brands fa-github"></i>
+            </span>
+            <span className={styles.projects_button} onClick={toggleTab}>
+              View more
+              <i class="fa-solid fa-arrow-right"></i>
+            </span>
+          </div>
+
+          <div
+            className={
+              toggleState
+                ? `${styles.projects_modal} ${styles.active_modal}`
+                : `${styles.projects_modal}`
+            }
+          >
+            <div className={styles.projects_modal_content}>
+              <div className={styles.porjects_modal_close} onClick={closeTab}>
+                <i class="fa-solid fa-xmark"></i>
+              </div>
+
+              <h3 className={styles.projects_modal_title}>Titlee heree</h3>
+              <p className={styles.projects_modal_description}>Description</p>
+
+              <ul className={`${styles.projects_modal_projects} grid`}>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>HTML</p>
+                </li>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>CSS</p>
+                </li>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>React</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={styles.projects_content}>
+          <div>
+            <i class="fa-solid fa-image"></i>
+
+            <h3 className={styles.projects_title}>Title Here</h3>
+          </div>
+
+          <div className={styles.projects_buttons_container}>
+            <span className={styles.projects_button}>
+              GitHub
+              <i class="fa-brands fa-github"></i>
+            </span>
+            <span className={styles.projects_button} onClick={toggleTab}>
+              View more
+              <i class="fa-solid fa-arrow-right"></i>
+            </span>
+          </div>
+
+          <div
+            className={
+              toggleState
+                ? `${styles.projects_modal} ${styles.active_modal}`
+                : `${styles.projects_modal}`
+            }
+          >
+            <div className={styles.projects_modal_content}>
+              <div className={styles.porjects_modal_close} onClick={closeTab}>
+                <i class="fa-solid fa-xmark"></i>
+              </div>
+
+              <h3 className={styles.projects_modal_title}>Titlee heree</h3>
+              <p className={styles.projects_modal_description}>Description</p>
+
+              <ul className={`${styles.projects_modal_projects} grid`}>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>HTML</p>
+                </li>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>CSS</p>
+                </li>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>React</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
