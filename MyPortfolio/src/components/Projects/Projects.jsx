41a8237bdf640a40ec7b427d9fcc27f785e993f9
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-01-16 12:50:40 +0200

    portfolio modal added

--- MyPortfolio/src/components/Projects/Projects.jsx
+++ MyPortfolio/src/components/Projects/Projects.jsx
@@ -5,104 +5,45 @@ export default function Projects() {
     <section className={`${styles.projects} section`}>
       <h2 className="section_title">Portfolio</h2>
       <span className="section_subtitles">Recent projects</span>
+
       <div className={`${styles.projects_container} container grid`}>
         <div className={styles.projects_content}>
           <div>
+            <i class="fa-solid fa-image"></i>
+
             <h3 className={styles.projects_title}>Title Here</h3>
           </div>
+
           <span className={styles.projects_button}>
             View more
             <i class="fa-solid fa-arrow-right"></i>
-            <div className={styles.projects_modal}>
-              <div className={styles.projecct_modals_content}>
-                <i class="fa-solid fa-arrows-up-down-left-right"></i>
-
-                <h3 className={styles.projects_title}>Titlee heree</h3>
-                <p className={styles.porjects_description}>Description</p>
-
-                <ul className={`${styles.projects_modal_project} grid`}>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
           </span>
-        </div>
 
-        <div className={styles.projects_content}>
-          <div>
-            <h3 className={styles.projects_title}>Title Here</h3>
+          <div className={styles.projects_modal}>
+            <div className={styles.projects_modal_content}>
+              <div className={styles.porjects_modal_close}>
+                <i class="fa-solid fa-xmark"></i>
+              </div>
+
+              <h3 className={styles.projects_modal_title}>Titlee heree</h3>
+              <p className={styles.projects_modal_description}>Description</p>
+
+              <ul className={`${styles.projects_modal_projects} grid`}>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>HTML</p>
+                </li>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>CSS</p>
+                </li>
+                <li className={styles.projects_modal_project}>
+                  <i class="fa-regular fa-circle-check"></i>
+                  <p className={styles.projects_modal_info}>React</p>
+                </li>
+              </ul>
+            </div>
           </div>
-          <span className={styles.projects_button}>
-            View more
-            <i class="fa-solid fa-arrow-right"></i>
-            <div className={styles.projects_modal}>
-              <div className={styles.projecct_modals_content}>
-                <i class="fa-solid fa-arrows-up-down-left-right"></i>
-
-                <h3 className={styles.projects_title}>Titlee heree</h3>
-                <p className={styles.porjects_description}>Description</p>
-
-                <ul className={`${styles.projects_modal_project} grid`}>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </span>
-        </div>
-
-        <div className={styles.projects_content}>
-          <div>
-            <h3 className={styles.projects_title}>Title Here</h3>
-          </div>
-          <span className={styles.projects_button}>
-            View more
-            <i class="fa-solid fa-arrow-right"></i>
-            <div className={styles.projects_modal}>
-              <div className={styles.projecct_modals_content}>
-                <i class="fa-solid fa-arrows-up-down-left-right"></i>
-
-                <h3 className={styles.projects_title}>Titlee heree</h3>
-                <p className={styles.porjects_description}>Description</p>
-
-                <ul className={`${styles.projects_modal_project} grid`}>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                  <li className={styles.projects_modal_project}>
-                    <i class="fa-regular fa-circle-check"></i>
-                    <p className={styles.projects_modal_info}>modal info</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </span>
         </div>
       </div>
     </section>
