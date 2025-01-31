1e3878129155c669835f930fed23395a1fc24bb8
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-01-31 22:08:51 +0200

    Footer

--- MyPortfolio/src/components/Footer/Footer.jsx
+++ MyPortfolio/src/components/Footer/Footer.jsx
@@ -1,5 +1,44 @@
 import styles from "./FooterStyles.module.css";
-
 export default function Footer() {
-  return <h1 className={styles.red}>Footer</h1>;
+  return (
+    <footer className={styles.footer}>
+      <div className={`${styles.footer_container} container`}>
+        <h1 className={styles.footer_title}>Emin</h1>
+
+        <ul className={styles.footer_list}>
+          <li>
+            <a href="#about" className={styles.footer_link}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className={styles.footer_link}>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#testemonials" className={styles.footer_link}>
+              Testemonials
+            </a>
+          </li>
+        </ul>
+
+        <div className={styles.footer_socials}>
+          <a className={styles.footer_social_link} target="_blank">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a className={styles.footer_social_link} target="_blank">
+            <i className="fa-brands fa-linkedin-in"></i>
+          </a>
+          <a className={styles.footer_social_link} target="_blank">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+        </div>
+
+        <span className={styles.footer_copy}>
+          &#169; All rights reserved - Emin Apturaim
+        </span>
+      </div>
+    </footer>
+  );
 }
