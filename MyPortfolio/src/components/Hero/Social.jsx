995dc2181a8c75117d754c298db6c0eaec411dd5
Emin A <122976155+eminn-a@users.noreply.github.com>
2025-02-09 18:20:59 +0200

    social links added

--- MyPortfolio/src/components/Hero/Social.jsx
+++ MyPortfolio/src/components/Hero/Social.jsx
@@ -3,13 +3,25 @@ import styles from "./HeroStyles.module.css";
 export default function Social() {
   return (
     <div className={styles.home_social}>
-      <a className={styles.home_social_icon} target="_blank">
+      <a
+        className={styles.home_social_icon}
+        target="_blank"
+        href="https://github.com/emin-apm"
+      >
         <i className="fa-brands fa-github"></i>
       </a>
-      <a className={styles.home_social_icon} target="_blank">
+      <a
+        className={styles.home_social_icon}
+        target="_blank"
+        href="https://www.linkedin.com/in/emin-apm/"
+      >
         <i className="fa-brands fa-linkedin-in"></i>
       </a>
-      <a className={styles.home_social_icon} target="_blank">
+      <a
+        className={styles.home_social_icon}
+        target="_blank"
+        href="https://www.instagram.com/emin_apm/"
+      >
         <i className="fa-brands fa-instagram"></i>
       </a>
     </div>
